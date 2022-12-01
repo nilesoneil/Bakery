278fe80048380965b0f5424895dfd94403a3437e
Niles O'Neil <nilesoneil@gmail.com>
2022-12-01 14:43:51 -0800

    working on test 4

--- NilesBakery.Tests/ModelTests/BreadStuffTests.cs
+++ NilesBakery.Tests/ModelTests/BreadStuffTests.cs
@@ -1,5 +1,7 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
 using NilesBakery.Models;
+using System;
 
 namespace NilesBakery.Tests
 {
@@ -16,19 +18,30 @@ namespace NilesBakery.Tests
     [TestMethod]
     public void GetDescription_ReturnsDescription_String()
     {
-      string description = "Crispy crunchy bread.";
+      string description = "Buy 2, get 1 free (every 3rd loaf of bread is free.). A single loaf costs $5, two loaves costs $10, and three loaves cost $10.";
       Bread newBread =  new Bread(description);
       string result = newBread.Description;
       Assert.AreEqual(description, result);
     }
 
     [TestMethod]
+    public void UserPrompt_UserInput_Int()
+    {
+      string userInput;
+      Console.Write("How much bread would you like?");
+      userInput = Console.ReadLine();
+      Console.WriteLine("'{0}' bread coming right up!", userInput);
+      Bread newBread = new Bread();
+    }
+
+
+    [TestMethod]
     public void SetDescription_SetDescription_String()  
     {
-      string description = "Crispy crunchy bread.";
+      string description = "Buy 2, get 1 free (every 3rd loaf of bread is free.). A single loaf costs $5, two loaves costs $10, and three loaves cost $10.";
       Bread newBread = new Bread(description);
 
-      string updatedDescription = "Do the dishes";
+      string updatedDescription = "Thanks for your purchase!";
       newBread.Description = updatedDescription;
       string result = newBread.Description;
 
